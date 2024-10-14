4baff0d0bc0d3f94a5431db613b73097020daed6
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2024-10-14 23:02:15 +0200

    fix contacts - searchbox

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
 
-const Contact = ({ contact, onDeleteContact }) => {
+const Contact = ({ contact }) => {
+    const dispatch = useDispatch();
+
+    const handleDelete = () => {
+        dispatch(deleteContact(contact.id));
+    };
+
     return (
         <li>
             {contact.name}: {contact.number}
-            <button onClick={() => onDeleteContact(contact.id)}>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
         </li>
     );
 };
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { changeFilter } from '../../redux/filtersSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter, selectNameFilter } from '../../redux/filtersSlice';
 import styles from './SearchBox.module.css';
 
 const SearchBox = () => {
     const dispatch = useDispatch();
+    const filter = useSelector(selectNameFilter);
 
     const handleFilterChange = (e) => {
         dispatch(changeFilter(e.target.value));
@@ -13,6 +14,7 @@ const SearchBox = () => {
     return (
         <input
             type="text"
+            value={filter}
             placeholder="Find contacts"
             onChange={handleFilterChange}
             className={styles.input}
